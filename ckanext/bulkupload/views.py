0a02e9cdc479d028d480524b9544a431dd6791bb
blagoja <blagojabozinovski@gmail.com>
2024-07-16 14:33:05 +0200

    Restructure templates

--- ckanext/bulkupload/views.py
+++ ckanext/bulkupload/views.py
@@ -55,7 +55,7 @@ def package_busoperator():
            'org_list': org_list,
         }
 
-        return base.render('package_busoperator.html', extra_var)
+        return base.render('package/package_busoperator.html', extra_var)
     
     elif flask.request.method == 'POST':
         context = {
