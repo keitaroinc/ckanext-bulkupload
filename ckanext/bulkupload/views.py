047e36c4350ee9ab968cfdea62aec13c5deaf933
stojanovskis1 <stojanovskis1@icloud.com>
2024-07-23 12:15:36 +0200

    Change s3filestore to env

--- ckanext/bulkupload/views.py
+++ ckanext/bulkupload/views.py
@@ -35,16 +35,16 @@ except:
     log.critical('''Please specify a ckan.storage_path in your config
                          for your uploads''')
     
-aws_access_key_id = config.get('ckanext.s3filestore.aws_access_key_id')
-aws_secret_access_key = config.get('ckanext.s3filestore.aws_secret_access_key')
-bucket = config.get('ckanext.s3filestore.aws_bucket_name')
-aws_region = config.get('ckanext.s3filestore.region_name')
+aws_access_key_id = os.getenv('CKANEXT__S3FILESTORE__AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('CKANEXT__S3FILESTORE__AWS_SECRET_ACCESS_KEY')
+bucket = os.getenv('CKANEXT__S3FILESTORE__AWS_BUCKET_NAME')
+aws_region = os.getenv('CKANEXT__S3FILESTORE__REGION_NAME')
 session = boto3.session.Session()
 s3_client = session.client(
     service_name='s3',
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key,
-    endpoint_url=config.get('ckanext.s3filestore.host_name', None),
+    endpoint_url=os.getenv('CKANEXT__S3FILESTORE__HOST_NAME', None),
     region_name=aws_region
 )
 
